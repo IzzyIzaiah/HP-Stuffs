663d603077640b06ffac3c75a1a8977f710ce2ec
Isaiah O <Izaiah@osmun.net>
2022-10-29 19:30:39 -0600

    Update App.js
    Started on making "Damage taken" and "Damage dealt" counters however they are broken due to trying to parse '+' to the beginning of the displayed value if its positive

--- src/App.js
+++ src/App.js
@@ -8,18 +8,25 @@ function App () {
   const [text, setText] = useState('')
   return (
     <Wrapper>
+      <div>
       <h1>Sentinals HP Tracker</h1>
+      <ListItem>Name</ListItem>
+      <ListItem>HP</ListItem>
+      <ListItem>Damage Recieved</ListItem>
+      <ListItem>Damage Dealt</ListItem>
+      </div>
       <MainBox>
         {counters.map((e, i) => (
           <div key={e.id}>
             <Counter name={e.name} />
-            <button
+            <StyledButton
+            className="npnm" 
               onClick={() => {
                 setCounters(counters => counters.filter(obj => obj.id !== e.id))
               }}
             >
               Delete
-            </button>
+            </StyledButton>
           </div>
         ))}
         <br />
@@ -38,7 +45,8 @@ function App () {
           }}
           onChange={e => setText(e.target.value)}
         />
-        <button
+        <StyledButton
+        className="npnm" 
           onClick={() => {
             setCounters(counters => [
               ...counters,
@@ -48,7 +56,7 @@ function App () {
           }}
         >
           Create New
-        </button>
+        </StyledButton>
       </MainBox>
     </Wrapper>
   )
@@ -56,29 +64,54 @@ function App () {
 
 function Counter ({ name }) {
   const [counter, setCounter] = useState(0)
-  //increase counter
+  // Damag Recieved (DR)
+  const [dr, setDR] = useState(0)
+  // Damage Dealt (DD)
+  const [dd, setDD] = useState(0)
+
+  //increase counters
   const increase = () => {
     setCounter(count => count + 1)
   }
+  const damageRecievedIncrease = () => {
+    setDR(dr => dr + 1)
+  }
+  const damageDealtIncrease = () => {
+    setDD(dd => dd + 1)
+  }
 
-  //decrease counter
+  //decrease counters
   const decrease = () => {
     setCounter(count => count - 1)
+  }
+  const damageRecievedDecrease= () => {
+    setDR(dr => dr - 1)
+  }
+  const damageDealtDecrease = () => {
+    setDD(dd => dd - 1)
   }
 
   //reset counter
   const reset = () => {
     setCounter(0)
+    setDD(0)
+    setDR(0)
   }
 
   return (
     <>
       <br />
       <Examble>{name}</Examble>
-      <button onClick={increase}>+</button>
+      <StyledButton onClick={increase}>+</StyledButton>
       <NameHolder>{counter}</NameHolder>
-      <button onClick={decrease}> -</button>
-      <button onClick={reset}>Reset</button>
+      <StyledButton className="minus" onClick={decrease}> -</StyledButton>
+      <StyledButton onClick={damageRecievedIncrease}>+</StyledButton>
+      <NameHolder>{dr}</NameHolder>
+      <StyledButton className="minus" onClick={damageRecievedDecrease}> -</StyledButton>
+      <StyledButton onClick={damageDealtIncrease}>+</StyledButton>
+      <NameHolder>{dd => {if(dd > 0) return `+${dd}`}}</NameHolder>
+      <StyledButton className="minus" onClick={damageDealtDecrease}> -</StyledButton>
+      <StyledButton className="npnm" onClick={reset}>Reset</StyledButton>
     </>
   )
 }
@@ -101,4 +134,44 @@ const NameHolder = styled.span`
 
 const Examble = styled.span`
   padding-right: 10px;
+  margin-left: 220px;
+  margin-right: 20px;
+  margin-bottom:5px;
+`
+
+const StyledButton = styled.button`
+cursor: pointer;
+outline: 0;
+text-align: center;
+background-color: transparent;
+border: 1px solid transparent;
+padding-left: 10px;
+padding-right: 10px;
+font-size: 1rem;
+border-radius: .25rem;
+transition: color .15s ease-in-out,background-color .15s ease-in-out,border-color .15s ease-in-out,box-shadow .15s ease-in-out;
+color: #0d6efd;
+height: 26px;
+:hover {
+    color: #fff;
+    background-color: #0d6efd;
+    border-color: #0d6efd;
+}
+&.npnm{
+  margin-left: 40px;
+margin-right: 40px;
+margin-bottom:5px;
+}
+&.minus{
+  margin-right:40px;
+}
+&.plus{
+  margin-left:40px
+}
+`
+
+const ListItem = styled.span`
+margin-left: 40px;
+margin-right: 40px;
+margin-bottom:5px;
 `
